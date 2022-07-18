60215f5c04fc1ccce6cec5eb937af4399a5a7cfe
cornelia247 <ekpenyongcornelia2@gmail.com>
2022-07-18 16:22:39 +0100

    added handlers and static files

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 	"strconv"
 )
@@ -12,7 +14,23 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 
 	}
-	w.Write([]byte("Hello from Mythos!"))
+	//home.page.tmpl must come first
+	files := []string{
+		"./ui/html/home.page.tmpl",
+		"./ui/html/base.layout.tmpl",
+		"./ui/html/footer.partial.tmpl",
+	}
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
 }
 
 func showMyth(w http.ResponseWriter, r *http.Request) {
